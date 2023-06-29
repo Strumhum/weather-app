eb968793a624add654097dd5eb5da0f1db79eaa3
Strumhum <sue@strumhumcreatives.com>
2023-06-29 23:38:46 +0100

    Added temperature conversion for Celsius or Fahrenheit

--- src/script.js
+++ src/script.js
@@ -1,50 +1,3 @@
-// display weather data on page for city
-function displayWeather(response) {
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#main-temp").innerHTML =
-    Math.round(response.data.main.temp) + "ºC";
-  document.querySelector("#humidity").innerHTML =
-    "Humidity: " + response.data.main.humidity + "%";
-  document.querySelector("#wind").innerHTML =
-    "Wind: " + Math.round(response.data.wind.speed) + " mph";
-  document.querySelector("#current-weather").innerHTML =
-    response.data.weather[0].main;
-}
-
-// get city by user input - city search
-function searchCity(city) {
-  let units = "metric";
-  let apiKey = "1a2b7258ebd456c01aef9175dfe8b709";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-  console.log(apiUrl);
-  axios.get(apiUrl).then(displayWeather);
-}
-
-function search(event) {
-  event.preventDefault();
-  let cityElement = document.querySelector("#city");
-  let cityInput = document.querySelector("#city-input");
-  cityElement.innerHTML = cityInput.value;
-
-  let city = cityInput.value;
-  searchCity(city);
-}
-
-// get city by geolocation - coordinates
-function showLocation(position) {
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
-  let units = "metric";
-  let apiKey = "1a2b7258ebd456c01aef9175dfe8b709";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(displayWeather);
-}
-
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(showLocation);
-}
-
 // get current date and time
 function showDate(date) {
   let hours = date.getHours();
@@ -69,17 +22,74 @@ function showDate(date) {
   return "".concat(day, " ").concat(hours, ":").concat(minutes);
 }
 
+// display weather data on page for city
+function displayWeather(response) {
+  let temperatureElement = document.querySelector("#main-temp");
+  let cityElement = document.querySelector("#city");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let currentWeatherElement = document.querySelector("#current-weather");
+
+  celsiusTemp = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemp) + "ºC";
+  cityElement.innerHTML = response.data.name;
+  humidityElement.innerHTML = "Humidity: " + response.data.main.humidity + "%";
+  windElement.innerHTML =
+    "Wind: " + Math.round(response.data.wind.speed) + " km/h";
+  currentWeatherElement.innerHTML = response.data.weather[0].main;
+}
+
+// get city by user input - city search
+function searchCity(city) {
+  let units = "metric";
+  let apiKey = "1a2b7258ebd456c01aef9175dfe8b709";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function search(event) {
+  event.preventDefault();
+  let cityElement = document.querySelector("#city");
+  let cityInput = document.querySelector("#city-input");
+  cityElement.innerHTML = cityInput.value;
+
+  let city = cityInput.value;
+  searchCity(city);
+}
+
+// get city by geolocation - coordinates
+function showLocation(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let units = "metric";
+  let apiKey = "1a2b7258ebd456c01aef9175dfe8b709";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showLocation);
+}
+
 // changing units of temperature by links
+function tempToFahrenheit(event) {
+  event.preventDefault();
+  let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
+  let temperatureElement = document.querySelector("#main-temp");
+  temperatureElement.innerHTML = Math.round(fahrenheitTemp) + "ºF";
+}
+
 function tempToCelsius(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#main-temp");
-  temperatureElement.innerHTML = "20ºC";
+  temperatureElement.innerHTML = Math.round(celsiusTemp) + "ºC";
 }
-function tempToFarenheit(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#main-temp");
-  temperatureElement.innerHTML = "68°F";
-}
+
+let celsiusTemp = null;
 
 // city search engine
 let searchForm = document.querySelector("#search-form");
@@ -94,8 +104,11 @@ let dateElement = document.querySelector("#current-day-time");
 let currentTime = new Date();
 dateElement.innerHTML = showDate(currentTime);
 
-// celsius and farenheit option
-let celsiusUnit = document.querySelector("#celsius-link");
-celsiusUnit.addEventListener("click", tempToCelsius);
-let farenheitUnit = document.querySelector("#farenheit-link");
-farenheitUnit.addEventListener("click", tempToFarenheit);
+// celsius and fahrenheit option
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", tempToFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", tempToCelsius);
+
+searchCity("London");
